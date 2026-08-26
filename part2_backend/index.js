9844cfc22c0222fd3754769cc3640bc4e299bd58
NhuVinhKhanhVo <bestplayerever112@gmail.com>
2026-08-26 15:22:23 +1000

    wubba bubba dub dub

--- part2_backend/index.js
+++ part2_backend/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const cors = require("cors")
+
 const app = express()
 
+
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors())
+
+
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
